65e3db20d51ded154a5999b69210a4f261eb9e7d
xBl0oM <111044215+xBl0oM@users.noreply.github.com>
2024-07-21 18:44:45 +0200

    Initial build/commit

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import Sidebar from './components/Sidebar';
+
+
+
+class App extends Component {
+    render() { 
+        return (
+                <div>
+                    <Sidebar/>
+                </div>
+        )
+    }
+}
+ 
+export default App;
---
+++ components/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import "../index.css";
+
+function Sidebar() {
+  return (
+    <div className="Sidebar"> Sidebar
+    </div>
+  )
+}
+
+export default Sidebar
