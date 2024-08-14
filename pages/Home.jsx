95f38cf3e204269e0310ee92386724c836c64137
Jan Frey <111044215+xBl0oM@users.noreply.github.com>
2024-08-14 13:13:55 +0200

    made responsive for phone

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,22 +1,21 @@
 import React from 'react';
 import "../index.css";
-import unihockeyBild from "../assets/unihockeyBild.png"
-
+import unihockeyBild from "../assets/unihockeyBild.png";
 
 function Home() {
   return (
-    <div className="ml-40 mr-40 ">
-      <div className="flex-grow  p-4 min-h-screen flex items-center">
-        <div className="flex flex-row items-center justify-center mx-auto">
-          <div className="ml-8 mr-12">
-            <h1 className="text-5xl font-playfair text-blue-900 -mb-2">Grüezi, mein name ist</h1> 
-            <h1 className="text-8xl font-playfair bg-gradient-to-r from-stone-500 to-cyan-500 bg-clip-text text-transparent mb-2 leading-normal">Jan Frey</h1> {/* Added leading-normal to adjust line-height */}
-            <h1 className="text-4xl">Schnell im Sport und im Programmieren</h1>
+    <div className="px-4 sm:px-8 md:px-16 lg:px-40 w-full bg-white">
+      <div className="flex-grow p-4 min-h-screen flex items-center">
+        <div className="flex flex-col md:flex-row items-center justify-center mx-auto">
+          <div className="mb-8 md:mb-0 md:ml-8 md:mr-12 text-center md:text-left">
+            <h1 className="text-3xl sm:text-4xl md:text-5xl font-playfair text-blue-900 -mb-2">Grüezi, mein Name ist</h1> 
+            <h1 className="text-5xl sm:text-6xl md:text-8xl font-playfair text-gray-800 bg-clip-text mb-2 leading-normal">Jan Frey</h1> 
+            <h1 className="text-2xl sm:text-3xl md:text-4xl font-playfair">Schnell im Sport und im Programmieren</h1>
           </div>
-          <img src={unihockeyBild} alt="Profilbild von Jan Frey" className="w-80 rounded-2xl mr-10 mt-4" />
+          <img src={unihockeyBild} alt="Profilbild von Jan Frey" className="w-40 sm:w-60 md:w-80 rounded-2xl mr-0 md:mr-10 mt-4" />
         </div>
       </div>
-      </div>
+    </div>
   );
 }
 
